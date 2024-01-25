42219fa99accfe5dd4fc707ead172b4e5ae695d8
lsaac <2423860442@qq.com>
2024-01-25 11:21:48 +0800

    通过socket 实现wordCount 读文件，无界流

--- src/main/java/com/echo/Main.java
+++ src/main/java/com/echo/Main.java
@@ -19,6 +19,7 @@ public class Main {
     public static void main(String[] args) throws Exception {
         wordCountBatchByDataSetApi();
         wordCountStream();
+        wordCountStreamUnbounded();
     }
 
     //基于DataSet API实现wordCount ,过时不推荐
@@ -49,6 +50,7 @@ public class Main {
         sum.print();
     }
 
+    //基于DataStream,有界流
     public static void wordCountStream() throws Exception {
         //1 创建环境
         StreamExecutionEnvironment env = StreamExecutionEnvironment.getExecutionEnvironment();
@@ -97,6 +99,27 @@ public class Main {
         //3> (hello,2)
         //3> (hello,3)
 
+    }
+
+
+    //读取无界流
+    public static  void wordCountStreamUnbounded() throws Exception{
+        //创建执行环境
+        StreamExecutionEnvironment env = StreamExecutionEnvironment.getExecutionEnvironment();
+        //socket读取数据
+        DataStreamSource<String> socketTextStream = env.socketTextStream("hadoop102", 7777);
+        //处理数据
+        SingleOutputStreamOperator<Tuple2<String, Integer>> sum = socketTextStream.flatMap((String value, Collector<Tuple2<String, Integer>> collector) -> {
+            String[] words = value.split(" ");
+            for (String word : words) {
+                collector.collect(Tuple2.of(word, 1));
+            }
+        }).returns(Types.TUPLE(Types.STRING,Types.INT)).keyBy((Tuple2<String, Integer> value) -> value.f0).sum(1);
+        //输出
+        sum.print();
+        //执行
+        env.execute();
+        // 事件驱动：有数据来才有下一步
     }
 
 
